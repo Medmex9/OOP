cff024c2df7d0fb0f9f0835e77ae5ea880c4076d
Тимофей <timon.from25@gmail.com>
2022-12-16 19:07:21 +0700

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    Car car1 = new Car("Lada","Granta", 1.7, "желтый", 0, "Россия" );
+    Car Car2 = new Car("Audi", "A8 50 L TDI quattro",
+            3.0, "черный", 2020, "Германия" );
+    Car car3 = new Car ("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+    Car car4 = new Car ("Kia", "Sportage",2.4,
+            "Красный", 2018, "Южная корея" );
+    Car car5 = new Car ("Hyundai", "Avante", 1.6,
+            "оранжевый", 2016, "Южная корея");
+
+        System.out.println(car1);
+
+
     }
+
 }
