a92b6158da5826a7d57d823d54a00ad2f2d43c28
Тимофей <timon.from25@gmail.com>
2022-12-21 20:29:00 +0700

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
+import transport.Car;
+
 public class Main {
     public static void main(String[] args) {
 
-    Car car1 = new Car("Lada","Granta", 1.7, "желтый", 0, "Россия" );
+    Car car1 = new Car("Lada","Granta", 1.7, "желтый", 0,
+            "Россия", "A", "sedan", 233, 4);
     Car Car2 = new Car("Audi", "A8 50 L TDI quattro",
-            3.0, "черный", 2020, "Германия" );
-    Car car3 = new Car ("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+            3.0, "черный", 2020, "Германия","A",
+            "sedan", 23432, 4 );
+    Car car3 = new Car ("BMW", "Z8", 3.0, "черный", 2021,
+            "Германия", "A", "sedan", 333, 4);
     Car car4 = new Car ("Kia", "Sportage",2.4,
-            "Красный", 2018, "Южная корея" );
+            "Красный", 2018, "Южная корея", "A", "sedan", 453, 4 );
     Car car5 = new Car ("Hyundai", "Avante", 1.6,
-            "оранжевый", 2016, "Южная корея");
+            "оранжевый", 2016, "Южная корея","A", "sedan", 675, 4);
 
         System.out.println(car1);
+        car1.new Key(false, false);
+
+
+
+
 
 
     }
---
+++ src/transport/Car.java
@@ -0,0 +1,108 @@
+package transport;
+
+import java.lang.annotation.Native;
+import java.util.Calendar;
+
+public class Car {
+    public class Key {
+        private boolean intremoteEngineStart;
+        private boolean engineStart;
+
+        public Key(boolean remoteEngineStart, boolean engineStart) {
+
+            this.intremoteEngineStart = false;
+            this.engineStart = false;}
+
+        public boolean getEngineStart() {return false;}
+        public boolean getRemoteEngineStart() {return false;}
+        public boolean getModel() {return false;}
+        public void setIntremoteEngineStart(boolean intremoteEngineStart) {
+            this.intremoteEngineStart = intremoteEngineStart;}
+        public void setEngineStart(boolean engineStart) {
+            this.engineStart = engineStart;
+        }
+
+
+    }
+        private final String brand;
+        private final String model;
+        private double engineVolume;
+        private String color;
+        private final int year;
+        private  final String country;
+
+        private String transmission;
+        private final String bodyType;
+        private int registrationNumber;
+        private final int numberOfSeats;
+        private int autoRubberWinter;
+        private Key key;
+
+        public Car(String brand, String model, double engineVolume, String color, int year, String country,
+                   String transmission, String bodyType, int registrationNumber, int numberOfSeats) {
+            if (transmission == null) { transmission = "MKPP";}
+            this.transmission = transmission;
+
+            if (bodyType == null) {bodyType = "Sedan";}
+            this.bodyType = bodyType;
+
+            if (registrationNumber == 0) { registrationNumber = 000;}
+            this.registrationNumber = registrationNumber;
+            if (numberOfSeats == 0) {numberOfSeats = 4;}
+            this.numberOfSeats = numberOfSeats;
+
+          if (brand == null) {brand = "default";}
+            this.brand = brand;
+
+          if (model == null) {model = "default";}
+            this.model = model;
+
+          if (country==null) {country ="default";}
+            this.country = country;
+
+          if (engineVolume<=0) {engineVolume = 1.5;}
+            this.engineVolume = engineVolume;
+
+          if (color == null) {color = "Белый";}
+            this.color =color;
+
+          if (year <=0) {year = 2000; }
+          this.year = year;}
+    public String getBrand() { return brand;}
+    public String getModel() {return model;}
+    public int getYear() {return year;}
+    public String getCountry() {return country;}
+    public String getBodyType() {return bodyType;}
+    public int getNumberOfSeats() {return numberOfSeats;}
+    public double getEngineVolume() {return engineVolume;}
+    public String getTransmission() {return transmission;}
+    public int getRegistrationNumber() { return registrationNumber;}
+    public void setRegistrationNumber(int registrationNumber) { this.registrationNumber = registrationNumber;}
+    public void setTransmission(String transmission) {this.transmission = transmission;}
+    public String getColor() {return color;}
+    public void setColor(String color) {this.color = color;}
+    public void setEngineVolume(double engineVolume) {this.engineVolume = engineVolume;}
+
+
+    public void setKey(boolean key) {
+        key = key;
+    }
+
+    public int AutoRubberWinter () {
+            Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH);
+        if (month<4||month>9) {
+            System.out.println("Машина на зимней резине");}
+        return autoRubberWinter;
+
+    }
+    @Override
+        public String toString() {
+                return "Марка " + brand + " модель " + model + " Объем двигателя " + engineVolume +
+                        " цвет " + color + " год производства " + year + " страна производитель " + country;
+
+}
+
+
+
+}
